3615bf1c7921087a0e8ad13971154770458ab2a7
Maiya2 <ss57063817@gmail.com>
2022-03-27 02:28:10 -0700

    first commit

---
+++ chap-16/assets/js/app.js
@@ -0,0 +1,18 @@
+var Months = ["Jan", "Feb" ,"mar", "apr","may","june", "july","aug","sep", "oct","nov","dec"]
+
+for (i=0; i < Months.length; i++) {
+     Months[i]=Months[i].toLowerCase()
+}
+console.log(Months)
+
+
+
+var Months = ["Jan", "Feb" ,"mar", "apr","may","june", "july","aug","sep", "oct","nov","dec"]
+
+for (i=0; i < Months.length; i++) {
+     Months[i]=Months[i].toUpperCase()
+}
+console.log(Months)
+
+
+
