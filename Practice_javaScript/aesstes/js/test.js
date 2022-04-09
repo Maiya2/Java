7482b51c87c9ed0f85b9be5c0f5de7d403d280db
Maiya2 <ss5706817@gmail.com>
2022-04-09 14:01:12 -0700

    first commit

---
+++ Practice_javaScript/aesstes/js/test.js
@@ -0,0 +1,64 @@
+// alert ("Hello")
+// document.write("<b>Hellow world<b/>")
+//>===========================================
+//Var 
+// var firstname , lastname , myage
+ 
+// firstname = " Sumaiya ";
+
+// lastname = "Shaikh";
+// myage = 24 ;
+    
+// document.write("My first Name is");
+// document.write(firstname);
+
+// document.write(" <br> <br> My last Name is");
+// document.write(lastname);
+
+// document.write(" <br> <br> My age ");
+// document.write( myage );
+// document.write(" yera Old");
+
+//>=============================================
+//Arithmetic Operators...
+// var  a = 10;
+// var b = 8;
+// a ++ ;
+
+// document.write(a);
+
+//>=================================================
+//Assignment Operators
+// var a = 6
+// var b = 3  
+//   a +=  b;
+
+// document.write(a);
+
+
+//>================================================
+//Deta type.....
+// var x = "Hafiza Sumaiya";
+// var x = 25;
+// var x = ["apple", "mango", "banana"]
+        
+// document.write("<br> <br>");
+// document.write(x);
+// document.write("<br> <br>");
+// document.write( typeof x);
+
+// >======================================================
+
+//If Statement..
+// var x = 5 ;
+// var y = 5 ;
+// if(x === y){
+//     document.write("this is true.")
+// }
+
+//>==============================================
+
+//Comparison Operators..
+// var x = 5;
+// var y = 5;
+// console.log(x  <= y)
