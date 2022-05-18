75f70fc8fa7f7a6d3f5a6263ff59545063f34f9c
Maiya2 <ss5706817@gmail.com>
2022-05-18 11:13:10 -0700

    first

---
+++ chap- 1 2  3 4/assets/js/text.js
@@ -0,0 +1,57 @@
+// alert("hello Miss Sumaiya");
+
+// var a = 30;
+// var b = 20;
+// if (a>b){
+//     alert("A is greater");
+// }else{
+//     alert("B is greater");
+// }
+
+//We can also concatinate
+// var a = 30;
+// var b = 20;
+// alert("value of A is: " + a)
+//we can perform mathemtic functions
+// var a = 10;
+// var b = 20;
+// alert(a+b);
+
+// var firstName = "Sumaiya"
+// var lastname = "Shaikh"
+// var age = "24"
+// alert("Hello "  + firstName + " " + lastname + " " + "You Are" + " " + age + " " + "years Old")
+
+//same work as alert/prompt...
+// var city = prompt("Where Do Yoy Live?");
+// document.write(city);
+
+// var city = prompt("Where Do Yoy Live?");
+// alert(city);
+
+//Confirm Box
+var  a = confirm("Do u like Our Website?");
+if (a) {
+    document.write("Thanks For your likesness");
+
+}else{
+    alert("Sorry We Will improve..");
+}
+
+
+// //>===============================================
+//  var firstname = "Sumaiya shaikh";
+// document.write(firstname + "<br>");
+ 
+// var firstNmae = "Abdul Majeed";
+// document.write(firstNmae);
+
+// let firstname = "Sumaiya shaikh";
+// document.write(firstname + "<br>");
+
+// let firstNmae = "Abdul Majeed";
+// document.write(firstNmae);
+
+// const salary = 500
+// const expences = 300;
+// document.write(expences + "<br>" + salary);
--- chap- 1 2 3/assets/js/text.js
+++
@@ -1,4 +0,0 @@
-var firstName = "sumaiya"
-var lastName = "shaikh"
-var age = "24"
-alert("Hello" + " " + firstName + " " + lastName + "You Are" + age + " "  + "year old")
