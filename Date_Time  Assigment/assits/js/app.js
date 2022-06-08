205fb6fb86aa416aff996b78fcb39694faca1a08
Maiya2 <ss5706817@gmail.com>
2022-06-08 18:17:29 +0500

    first

--- Date_Time  Assigment/assits/js/app.js
+++
@@ -1,7 +0,0 @@
-function clocktiming(){
-var d = new Date ();
-var Sumi = d.toLocaleTimeString();
-document.getElementById('ShowClock').innerHTML = Sumi;
-}
-clocktiming();
-setInterval(clocktiming, 1000);
